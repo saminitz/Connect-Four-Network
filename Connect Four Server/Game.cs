af46df3224434a0695894d4f69e07e4d087ee4e1
saminitz <samuel.nitzsche+github@gmail.com>
2022-07-06 20:04:43 +0200

    create empty project

---
+++ Connect Four Server/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect_Four_Server
+{
+    internal class Game
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
