6ffdca56e3c5b60fec6e2e92664df8dfda1d52a3
saminitz <samuel.nitzsche+github@gmail.com>
2022-07-06 20:50:10 +0200

    add first attributes and methods

--- Connect Four Server/Game.cs
+++ Connect Four Server/Game.cs
@@ -8,8 +8,24 @@ namespace Connect_Four_Server
 {
     internal class Game
     {
-        static void Main(string[] args)
+        private const int _gridSizeX = 7;
+        private const int _gridSizeY = 6;
+        private int[,] _grid;
+        private int _currentPlayer;
+
+        private int GetNextYPos(int x)
         {
+            throw new NotImplementedException();
+        }
+
+        private int CheckForWin()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PlayerAction(int player, int xPos)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# four wins tcp
-four wins with an implemented tcp connection
+# Connect Four Network
+Connect four with a client server implementation which are communicating via a WebSocket
